76ceba98c36a2b24b3d087771ae3abecfb13b94c
tsclausing <tsclausing@gmail.com>
2013-01-12 09:48:19 -0600

    Initial commit for puzzle package.

---
+++ src/puzzle/__init__.py
@@ -0,0 +1,7 @@
+"""
+The puzzle package contains modules which describe and solve various programming puzzles.
+
+Each puzzle module implements the following interface:
+    module.puzzle(inputs ...) -> returns the puzzle
+    module.solve(puzzle) -> returns the solution(s)
+"""
